e04af1de5f9c3e4e9dacb2ce3f8fff5e4e531513
Douglas Silveira <dgsilveira@sga.pucminas.br>
2023-11-09 09:09:18 -0300

    Chave estrangeira não nula (integridade referencial)

--- VendasWebMvc/Models/Vendedor.cs
+++ VendasWebMvc/Models/Vendedor.cs
@@ -12,6 +12,7 @@ namespace VendasWebMvc.Models
         public DateTime DataNascimento { get; set; }
         public double SalarioBase { get; set; }
         public Departamento Departamento { get; set; }
+        public int DepartamentoId { get; set; }
         public ICollection<RegistroVenda> Vendas { get; set; } = new List<RegistroVenda>();
 
         public Vendedor()
