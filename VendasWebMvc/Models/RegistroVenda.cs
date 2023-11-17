1ae2e5a5208c5d591d133e69b1bb51974e2d7d61
Douglas Silveira <dgsilveira@sga.pucminas.br>
2023-11-17 10:50:12 -0300

    Implementando busca agrupada

--- VendasWebMvc/Models/RegistroVenda.cs
+++ VendasWebMvc/Models/RegistroVenda.cs
@@ -10,6 +10,7 @@ namespace VendasWebMvc.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Valor { get; set; }
         public StatusVenda Status { get; set; }
         public Vendedor Vendedor { get; set; }
